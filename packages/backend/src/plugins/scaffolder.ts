cb207f087242b5c33b5eaa57f59f855a69da928d
root <root@ip-172-31-21-54.ec2.internal>
2025-07-09 11:26:48 +0000

    first commit new

---
+++ packages/backend/src/plugins/scaffolder.ts
@@ -0,0 +1,20 @@
+const actions = [
+  createHttpBackstageAction({ config }),
+  ...createBuiltinActions({
+    containerRunner,
+    integrations,
+    config,
+    catalogClient,
+    reader,
+  }),
+];
+
+return await createRouter({
+  containerRunner,
+  logger,
+  config,
+  database,
+  catalogClient,
+  reader,
+  actions,
+});
